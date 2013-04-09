88b6e2a3922511d38b6263e3a549d09171c313ca
Micah Woods <micahwoods@gmail.com>
2013-04-09 00:14:39 -0500

    Parsing url's is good

--- lib/rippersnapper/url.rb
+++ lib/rippersnapper/url.rb
@@ -1,3 +1,21 @@
 class Url
-  attr_accessor :subdomain, :domain, :suffix, :scheme, :host, :path, :url
+  attr_accessor :subdomain, :domain, :suffix, :scheme, :host
+
+  def initialize url
+    @url = url
+  end
+
+  def uri
+    @uri ||= URI.parse url
+  end
+
+  def url
+    return @url if @url =~ /:\/\//
+    "http://#{@url}"
+  end
+
+  def path
+    return "#{uri.path}?#{uri.query}" if uri.query
+    uri.path
+  end
 end
--- spec/rippersnapper/url_spec.rb
+++ spec/rippersnapper/url_spec.rb
@@ -3,6 +3,8 @@ require 'spec_helper'
 module Rippersnapper
 
   describe Url do
+    subject { Url.new "www.google.com" }
+
     it { should respond_to :domain }
     it { should respond_to :scheme }
     it { should respond_to :host }
@@ -10,5 +12,23 @@ module Rippersnapper
     it { should respond_to :url }
     it { should respond_to :suffix }
     it { should respond_to :subdomain }
+
+
+    context "with a scheme" do
+      subject { Url.new "http://drive.google.com/micah" }
+      its(:url) { should eq "http://drive.google.com/micah" }
+      its(:path) { should eq "/micah" }
+    end
+
+    context "without a scheme" do
+      subject { Url.new "www.google.com/micah" }
+      its(:url) { should eq "http://www.google.com/micah" }
+      its(:path) { should eq "/micah" }
+    end
+
+    context "with a query string" do
+      subject { Url.new "www.google.com/micah?date=today" }
+      its(:path) { should eq "/micah?date=today" }
+    end
   end
 end
