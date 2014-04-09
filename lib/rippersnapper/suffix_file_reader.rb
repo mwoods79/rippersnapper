9762b3f012d279a0d0e2aa3feac821bd9b643798
Jonathan Jackson <jonathan.jackson1@me.com>
2014-04-09 16:36:38 -0400

    Document suffix file reader class

--- lib/rippersnapper/suffix_file_reader.rb
+++ lib/rippersnapper/suffix_file_reader.rb
@@ -1,18 +1,23 @@
 module Rippersnapper
-
-  ###
-  # Single Responsibility:
-  #   Finds a suffix match in a list of suffixes
-
   class SuffixFileReader
     attr_reader :file
 
+    # Reads in the public_suffix and populates public_suffixes for use in
+    # #contains
+    #
+    # @params file [File] file object containing public suffixes
+    # @return [self]
     def initialize file = nil
       @file = file || File.open(File.dirname(__FILE__) + "/public_suffix.dat", 'r')
       @public_suffixes = Hash.new { false }
       parse_file
     end
 
+    # Used to determin if a given suffix is present in the public_suffix data
+    # file
+    # @param suffix [String] the suffix which will be checked against the
+    # public_suffix file @return [Boolean] true or false depending on inclusion
+    # in suffix file
     def contains? suffix
       @public_suffixes[suffix] || begin
         suffix_parts = suffix.split('.')
@@ -24,8 +29,9 @@ module Rippersnapper
 
     private
 
-    # NOTE: This could be extracted into an object if something more robust is
-    # needed in the future
+    # Populates the @public_suffixes instance var.  Each line is set to a key
+    # within the variable unless it starts with a // (slash slash)
+    # @return [void]
     def parse_file
       file.each_line do |line|
         line = line.strip
