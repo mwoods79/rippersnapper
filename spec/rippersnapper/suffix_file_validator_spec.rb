b54b6693090267684e774d630bda67c54b418fbd
Nick Palaniuk <npalaniuk@gmail.com>
2015-09-26 16:36:15 -0400

    Bump rspec and update syntax

--- spec/rippersnapper/suffix_file_validator_spec.rb
+++ spec/rippersnapper/suffix_file_validator_spec.rb
@@ -8,56 +8,64 @@ module Rippersnapper
     describe '#initialize' do
 
       context 'passing in a file' do
-        let(:file) { stub :file, each_line: [] }
+        let(:file) { double :file, each_line: [] }
         subject { SuffixFileReader.new file }
-        its(:file) { should be file}
+
+        describe '#file' do
+          subject { super().file }
+          it { is_expected.to be file}
+        end
       end
 
       context 'file default' do
         subject { SuffixFileReader.new }
-        its(:file) { should be_a_kind_of File }
+
+        describe '#file' do
+          subject { super().file }
+          it { is_expected.to be_a_kind_of File }
+        end
       end
     end
 
     describe '#contains?' do
       subject { SuffixFileReader.new }
 
-      it { should respond_to :contains? }
+      it { is_expected.to respond_to :contains? }
 
       context "matching valid data" do
 
         it 'matches a single suffix' do
-          expect(subject.contains?("com")).to be_true
+          expect(subject.contains?("com")).to be_truthy
         end
 
         it 'matches a nested suffix' do
-          expect(subject.contains?("la.us")).to be_true
+          expect(subject.contains?("la.us")).to be_truthy
         end
 
         it 'matches a suffix with a single asterix' do
-          expect(subject.contains?("whatever.uk")).to be_true
+          expect(subject.contains?("whatever.uk")).to be_truthy
         end
 
         it 'matches a suffix with multiple asterix' do
-          expect(subject.contains?("whatever.sapporo.jp")).to be_true
+          expect(subject.contains?("whatever.sapporo.jp")).to be_truthy
         end
       end
 
       context "rejecting invalid data" do
         it "doesn't match suffix that is not in file" do
-          expect(subject.contains?("baz")).to be_false
+          expect(subject.contains?("baz")).to be_falsey
         end
 
         it "doesn't match complex suffix that are not in file" do
-          expect(subject.contains?("doesnt.exist.sapporo.jp")).to be_false
+          expect(subject.contains?("doesnt.exist.sapporo.jp")).to be_falsey
         end
 
         it "doesn't match comments" do
-          expect(subject.contains?("// comment")).to be_false
+          expect(subject.contains?("// comment")).to be_falsey
         end
 
         it "doesn't match empty lines" do
-          expect(subject.contains?("")).to be_false
+          expect(subject.contains?("")).to be_falsey
         end
       end
     end
